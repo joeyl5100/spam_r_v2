05d9c2b3a1cb29d9266896ca6f9f261a2016bb57
Joey Lederman <lederman17@grinnell.edu>
2017-02-23 03:16:32 +0000

    Working home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
- 
   before_action :authenticate_user!
- 
-end
 
 def after_sign_in_path_for(resource)
-    app_dashboard_index_path
+    get 'static_pages/home'
+end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_user!, :only => [:index]
+ # skip_before_action :authenticate_user!, :only => [:index]
   #not sure  	
-load_and_authorize_resource
-#not sure
+# load_and_authorize_resource
+  #not sure
   def home
   end
 end
