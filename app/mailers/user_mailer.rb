d761bbead5b5962e838215f6cfe3d78a552c136d
Joey Lederman <lederman17@grinnell.edu>
2017-04-24 20:27:01 +0000

    New subject format

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -21,13 +21,13 @@ class UserMailer < ApplicationMailer
         if mail.from[0].include? ("@grinnell.edu")
           message = Message.new
           message.id = id
-          message.subject = mail.subject
+          #Grab subject that doesn't include csstudent and other tags
+          message.subject = addTag(message, mail.subject).squeeze(" ")
+          message.tag_list.sort!
           message.author = mail.from[0]
           message.content = getContent(mail)
           message.created_at = mail.date.to_s
           message.updated_at = Time.now.strftime("%Y-%m-%d %H:%M")
-          addTag(message)
-          message.tag_list.sort!
           saveAttachments(mail)
           message.save
           id += 1
@@ -37,9 +37,9 @@ class UserMailer < ApplicationMailer
   end
   
   # remove csstudent and tags from the subject
-  def trimSubject(message)
-    noCss = message.subject.sub("[csstudents]", "")
-    noTag = noCss.sub(/\{.*\}\s/, "").strip
+  def trimSubject(subject)
+    noTag = subject.sub(/[\[].*[\]]/, "").strip
+    return noTag
   end
   
   def getContent(mail)
@@ -52,13 +52,14 @@ class UserMailer < ApplicationMailer
   end
   
   # Add tags using subject
-  # Grabs text between {}
-  def addTag(message)
-    sub = message.subject.downcase
-    tags = /[{].*[}]/.match(sub)
+  # Grabs tags between brackets and trims the subject
+  def addTag(message, mail)
+    subject = mail.sub("[csstudents]", "")
+    subjectLow = subject.downcase
+    tags = /[\[].*[\]]/.match(subjectLow)
     if !tags.nil?
       tags = tags[0][1..-2]
-      tagArr = tags.split(',')
+      tagArr = tags.split('][')
       tagArr.each do |tag|
        if tag.include? "cs extra"
           message.tag_list.add("CS Extra")
@@ -75,6 +76,7 @@ class UserMailer < ApplicationMailer
     else
       message.tag_list.add("Misc.")
     end
+    return subject.sub(/[\[].*[\]]/, "").strip
   end
 
 def saveAttachments(mail)
