212d6ebb47fc01a0cc1f38ed3b5257fc57320d7c
Chiara Zizza <zizzachi@grinnell.edu>
2017-05-03 19:01:55 +0000

    Removed non-DRY html pages and reformatted Home page

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,7 +3,7 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
 
 #Grab mail from gmail every hour
-scheduler.every '1h' do
+scheduler.every '1m' do
   mailer = UserMailer.new
   mailer.getMail     
 end
