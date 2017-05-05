3b0235355c1aa51af919fe00bbbc50e5b6668c78
Adam Wesely <weselyad17@grinnell.edu>
2017-05-05 19:52:19 +0000

    heroku situation

--- features/step_definitions/paths.rb
+++ features/step_definitions/paths.rb
@@ -8,6 +8,8 @@ module NavigationHelpers
             new_user_session_path
         when /Register/
             new_user_registration_path
+        when /Messages/
+            messages_url
         when /Resend Confirmation/
             #root_path + /users/confirmation/new
             #new_user_confirmation #may need to change
@@ -25,6 +27,16 @@ module NavigationHelpers
             messages_url << "?tag=Candidate"
         when /Misc Search/
             messages_url << "?tag=Misc."
+        when /Show/
+            messages_url << "/1"
+        when /Second/
+            messages_url << "?page=2"
+        when /First/
+            messages_url << "?page=1"
+        when /Most Recent Message/
+            messages_url << "/15"
+        when /Faulty Tag/
+            messages_url << "?tag=aoesunth.nstphsnt.,hpnstabeusneotasntuhsnaeou"
          when /Example1 Search/   
             messages_url << "?utf8=✓&q%5Bauthor_cont%5D=example1&q%5Bsubject_cont%5D=&q%5Bcontent_cont%5D=&commit=Search"
         else
