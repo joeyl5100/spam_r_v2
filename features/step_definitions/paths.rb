bbf400a7bc52af4c26635ba6e073b47361a810d1
Chiara Zizza <zizzachi@grinnell.edu>
2017-05-05 19:53:37 +0000

    Added more tests

--- features/step_definitions/paths.rb
+++ features/step_definitions/paths.rb
@@ -9,8 +9,7 @@ module NavigationHelpers
         when /Register/
             new_user_registration_path
         when /Resend Confirmation/
-            #root_path + /users/confirmation/new
-            #new_user_confirmation #may need to change
+
         when /Talk Search/
             messages_url << "?tag=Talk"
         when /CS Table Search/
@@ -25,8 +24,12 @@ module NavigationHelpers
             messages_url << "?tag=Candidate"
         when /Misc Search/
             messages_url << "?tag=Misc."
-         when /Example1 Search/   
-            messages_url << "?utf8=✓&q%5Bauthor_cont%5D=example1&q%5Bsubject_cont%5D=&q%5Bcontent_cont%5D=&commit=Search"
+        when /Example One Search/   
+            messages_url << "?q%5Bauthor_cont%5D=example1&q%5Bsubject_cont%5D=&q%5Bcontent_cont%5D=&q%5Bcreated_at_gteq%5D=&q%5Bcreated_at_lteq%5D=&commit=Search"
+        when /Example Two Search/   
+            messages_url << "?q%5Bauthor_cont%5D=&q%5Bsubject_cont%5D=Example2&q%5Bcontent_cont%5D=&q%5Bcreated_at_gteq%5D=&q%5Bcreated_at_lteq%5D=&commit=Search"
+        when /Example Three Search/
+            messages_url << "?q%5Bauthor_cont%5D=&q%5Bsubject_cont%5D=&q%5Bcontent_cont%5D=Content3&q%5Bcreated_at_gteq%5D=&q%5Bcreated_at_lteq%5D=&commit=Search"
         else
             raise "Can't find mapping from \"#{page_name}\" to a path.\n"
         end
